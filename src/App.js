de77388e96a901b54bf7d0d24fda5d64279d71c9
CIOC <you@example.com>
2018-11-15 13:01:24 +0530

    toggle using js

--- src/App.js
+++ src/App.js
@@ -37,26 +37,20 @@ class App extends Component {
   }
 
   render() {
-
     const style = {
       backgroundColor:'white',
       font :'inherit',
       border:'1px solid blue',
       padding:'8px',
       cursor:'pointer',
-      boxShadow:'0px 2px 6px blue'
+      boxShadow:'0px 2px 4px blue'
 
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p>This is really working</p>
-        <button
-          style={style}
-         onClick={this.togglePersonsHandler}>Switch Name
-        </button>
-        { this.state.showPersons ?
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
         <div>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
@@ -64,8 +58,19 @@ class App extends Component {
            changed={this.nameChangeHandler}
           >My Hobbies : Racing!</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        </div> : null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working</p>
+        <button
+          style={style}
+         onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
 
       </div>
     );
